eae4cd2771f4678b6b9f9a8f7552df55dd12d63c
dingnan <2702288406@qq.com>
2022-05-13 15:37:00 +0800

    日志更新

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ np.random.seed(555)
 
 parser = argparse.ArgumentParser()
 
-'''
+
 # movie
 parser.add_argument('--dataset', type=str, default='movie', help='which dataset to use')
 parser.add_argument('--aggregator', type=str, default='sum', help='which aggregator to use')
@@ -21,7 +21,7 @@ parser.add_argument('--batch_size', type=int, default=65536, help='batch size')
 parser.add_argument('--l2_weight', type=float, default=1e-7, help='weight of l2 regularization')
 parser.add_argument('--lr', type=float, default=2e-2, help='learning rate')
 parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
-'''
+
 
 '''
 # book
@@ -37,7 +37,7 @@ parser.add_argument('--lr', type=float, default=2e-4, help='learning rate')
 parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
 '''
 
-
+'''
 # music
 parser.add_argument('--dataset', type=str, default='music', help='which dataset to use')
 parser.add_argument('--aggregator', type=str, default='sum', help='which aggregator to use')
@@ -49,7 +49,7 @@ parser.add_argument('--batch_size', type=int, default=128, help='batch size')
 parser.add_argument('--l2_weight', type=float, default=1e-4, help='weight of l2 regularization')
 parser.add_argument('--lr', type=float, default=5e-4, help='learning rate')
 parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
-
+'''
 
 
 show_loss = False
